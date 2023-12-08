f0cb87b9c414ef71f95e7babb02e343db9faacec
IvanKrutik <ivan.krutik@gmail.com>
2023-12-08 21:56:09 +0400

    1. Two Sum

---
+++ 1. Two Sum/Helper.cs
@@ -0,0 +1,25 @@
+﻿namespace _1._Two_Sum
+{
+    public static class Helper
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = 1; j < nums.Length; j++)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    if ((nums[i] + nums[j]) == target)
+                    {
+                        return new int[2] { i, j };
+                    }
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ 1. Two Sum/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using _1._Two_Sum;
+
+var nums = new int[4] { 2, 7, 11, 15 };
+int target = 9;
+
+var res = Helper.TwoSum(nums, target);
+
+foreach (var x in res)
+{
+    Console.WriteLine(x);
+}
