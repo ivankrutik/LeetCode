13d0c0573da5b2cc06d86b671895aa324568cbd4
IvanKrutik <ivan.krutik@gmail.com>
2023-12-08 21:11:21 +0400

    14. Longest Common Prefix

---
+++ 14. Longest Common Prefix/Helpre.cs
@@ -0,0 +1,43 @@
+﻿namespace _14._Longest_Common_Prefix
+{
+
+    public static class Helper
+    {
+        public static string LongestCommonPrefix(string[] strs)
+        {
+            var sortedList = new SortedList<string, int>();
+
+            for (var i = 0; i < strs.Count(); i++)
+            {
+                if (!sortedList.ContainsKey(strs[i]))
+                {
+                    sortedList.Add(strs[i], i);
+                }
+
+            }
+
+            var index = 0;
+            char currentChar;
+            string res = string.Empty;
+
+            //var word = sortedList.GetKeyAtIndex(0).ToString();
+            var word = sortedList.FirstOrDefault().Key.ToString();
+            foreach (var charInWord in word.ToCharArray())
+            {
+                currentChar = charInWord;
+
+                foreach (var str in sortedList)
+                {
+                    if (str.Key[index] != currentChar)
+                    {
+                        return res;
+                    }
+                }
+                res += charInWord;
+                index++;
+            }
+
+            return res;
+        }
+    }
+}
---
+++ 14. Longest Common Prefix/Program.cs
@@ -0,0 +1,10 @@
+﻿using _14._Longest_Common_Prefix;
+
+
+//var strs = new string[3] { "dog", "racecar", "car" };
+var strs = new string[4] { "flower", "flower" , "flow", "flight" };
+
+
+var res = Helper.LongestCommonPrefix(strs);
+
+Console.WriteLine(res);
