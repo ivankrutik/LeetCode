af1595026ec3da7f89a75b8bd4fba6d5e8fb8eb8
IvanKrutik <ivan.krutik@gmail.com>
2023-12-12 18:17:25 +0400

    9. Palindrome Number

---
+++ 9. Palindrome Number/Helpre.cs
@@ -0,0 +1,24 @@
+﻿namespace _09._Palindrome_Number
+{
+
+    public static class Helper
+    {
+        public static bool IsPalindrome(int x)
+        {
+            if (x < 0)
+            {
+                return false;
+            }
+            var str = x.ToString();
+            var res = string.Empty;
+
+            int n = str.Length;
+            for (int i = n; i != 0; i--)
+            {
+                res += str[i - 1];
+            }
+
+            return str == res;
+        }
+    }
+}
