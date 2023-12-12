5a99ca271236e63ac7d3374355687beb130a23ff
IvanKrutik <ivan.krutik@gmail.com>
2023-12-12 18:43:30 +0400

    13. Roman to Integer

---
+++ 13. Roman to Integer/Helper.cs
@@ -0,0 +1,44 @@
+﻿namespace _13._Roman_to_Integer
+{
+    internal static class Helper
+    {
+        private static Dictionary<char, int> _dictLiter = new Dictionary<char, int>(7)
+        {
+            { 'I', 1 },
+            { 'V', 5 },
+            { 'X', 10 },
+            { 'L', 50 },
+            { 'C', 100 },
+            { 'D', 500 },
+            { 'M', 1000 }
+        };
+
+        public static int RomanToInt(string s)
+        {
+            int res = 0;
+
+            for (var i = 0; i < s.Length; i++)
+            {
+                /*определим +/-*/
+                if ((i + 1) < s.Length &&
+                    ((s[i] == 'I' && (s[i + 1] == 'V' || s[i + 1] == 'X'))
+                    ||
+                    (s[i] == 'X' && (s[i + 1] == 'L' || s[i + 1] == 'C'))
+                     ||
+                    (s[i] == 'C' && (s[i + 1] == 'D' || s[i + 1] == 'M'))
+                    ))
+                //-
+                {
+                    res = res - _dictLiter[s[i]];
+                }
+                //+
+                else
+                {
+                    res = res + _dictLiter[s[i]];
+                }
+            }
+
+            return res;
+        }
+    }
+}
--- 14. Longest Common Prefix/Program.cs
+++ 14. Longest Common Prefix/Program.cs
@@ -1,5 +1,30 @@
 ﻿using _14._Longest_Common_Prefix;
 
+/*
+ Write a function to find the longest common prefix string amongst an array of strings.
+
+If there is no common prefix, return an empty string "".
+
+ 
+
+Example 1:
+
+Input: strs = ["flower","flow","flight"]
+Output: "fl"
+Example 2:
+
+Input: strs = ["dog","racecar","car"]
+Output: ""
+Explanation: There is no common prefix among the input strings.
+ 
+
+Constraints:
+
+1 <= strs.length <= 200
+0 <= strs[i].length <= 200
+strs[i] consists of only lowercase English letters.
+ */
+
 
 //var strs = new string[3] { "dog", "racecar", "car" };
 var strs = new string[4] { "flower", "flower" , "flow", "flight" };
