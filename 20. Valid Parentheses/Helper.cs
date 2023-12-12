09f457bb1db037288c7c1882190534e1cfe6d2be
IvanKrutik <ivan.krutik@gmail.com>
2023-12-12 18:47:39 +0400

    20. Valid Parentheses

---
+++ 20. Valid Parentheses/Helper.cs
@@ -0,0 +1,46 @@
+﻿namespace _20._Valid_Parentheses
+{
+    internal static class Helper
+    {
+        public static bool IsValid(string s)
+        {
+            if (s.Length % 2 == 1)
+            {
+                return false;
+            }
+
+            Stack<char> st = new Stack<char>();
+            foreach (char c in s)
+            {
+                try
+                {
+                    var prev = st.Peek();
+
+                    if (prev == '[' && c == ']')
+                    {
+                        st.Pop();
+                    }
+                    else if (prev == '(' && c == ')')
+                    {
+                        st.Pop();
+                    }
+                    else if (prev == '{' && c == '}')
+                    {
+                        st.Pop();
+                    }
+                    else
+                    {
+                        st.Push(c);
+                    }
+                }
+                catch
+                {
+                    st.Push(c);
+                    continue;
+                }
+            }
+
+            return st.Count == 0;
+        }
+    }
+}
