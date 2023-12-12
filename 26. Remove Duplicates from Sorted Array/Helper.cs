564b577bec89d1cacc256b2212abb75dec973da8
IvanKrutik <ivan.krutik@gmail.com>
2023-12-12 20:10:56 +0400

    26. Remove Duplicates from Sorted Array

---
+++ 26. Remove Duplicates from Sorted Array/Helper.cs
@@ -0,0 +1,21 @@
+﻿namespace _26._Remove_Duplicates_from_Sorted_Array
+{
+    public static class Helper
+    {
+        public static int RemoveDuplicates(int[] nums)
+        {
+            int j = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if ((i < nums.Length - 1) && nums[i] == nums[i + 1])
+                {
+                    continue;
+                }
+
+                nums[j++] = nums[i];
+            }
+            return j;
+        }
+    }
+}
