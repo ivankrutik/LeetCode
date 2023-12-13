f58c81930031e5c72c6b7ed1fb25e25b06203052
IvanKrutik <ivan.krutik@gmail.com>
2023-12-13 17:11:19 +0400

    27. Remove Element

---
+++ 27. Remove Element/Helper.cs
@@ -0,0 +1,36 @@
+﻿namespace _27._Remove_Element
+{
+    internal static class Helper
+    {
+        public static int RemoveElement(int[] nums, int val)
+        {
+            int position = 0;
+            int j = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != val)
+                {
+                    position++;
+                }
+                else
+                {
+                    j = position+1;
+                    while (j < nums.Length)
+                    {
+                        if (nums[j] != val)
+                        {
+                            nums[i] = nums[j];
+                            nums[j] = val;
+
+                            position++;
+                            break;
+                        }
+                        j++;
+                    }
+                }
+            }
+
+            return position;
+        }
+    }
+}
