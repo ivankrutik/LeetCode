25916f4fac43f679a63f46439125aab170a0a6b7
IvanKrutik <ivan.krutik@gmail.com>
2023-12-14 19:10:15 +0400

    35. Search Insert Position - BruteForce!

---
+++ 35. Search Insert Position/Helper.cs
@@ -0,0 +1,28 @@
+﻿namespace _35._Search_Insert_Position
+{
+    internal static class Helper
+    {
+        internal static int SearchInsert(int[] nums, int target)
+        {
+            var i = Array.IndexOf(nums, target);
+            if (i != -1)
+                return i;
+
+            i = 0;
+
+            foreach (var item in nums)
+            {
+                if (target > item)
+                {
+                    i++;
+                }
+                else
+                {
+                    return i;
+                }
+            }
+
+            return i;
+        }
+    }
+}
