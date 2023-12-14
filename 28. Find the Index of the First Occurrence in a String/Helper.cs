726ce0a6847e13c3f32460b217f36e4e8f1e51e2
IvanKrutik <ivan.krutik@gmail.com>
2023-12-14 18:37:05 +0400

    28. Find the Index of the First Occurrence in a String
    Алгоритм Кнута — Морриса — Пратта (КМП-алгоритм)
    Непростое решение!

---
+++ 28. Find the Index of the First Occurrence in a String/Helper.cs
@@ -0,0 +1,63 @@
+﻿using static System.Net.Mime.MediaTypeNames;
+
+namespace _28._Find_the_Index_of_the_First_Occurrence_in_a_String
+{
+    internal static class Helper
+    {
+        static int[] GetPrefix(string s)
+        {
+            int[] result = new int[s.Length];
+            result[0] = 0;
+            int index = 0;
+
+            for (int i = 1; i < s.Length; i++)
+            {
+                int k = result[i - 1];
+                while (s[k] != s[i] && k > 0)
+                {
+                    k = result[k - 1];
+                }
+                if (s[k] == s[i])
+                {
+                    result[i] = k + 1;
+                }
+                else
+                {
+                    result[i] = 0;
+                }
+            }
+            return result;
+        }
+
+
+        public static int StrStr(string haystack, string needle)
+        {
+            if (haystack == null || needle == null)
+            {
+                return -1;
+            }
+
+            int[] pf = GetPrefix(needle);
+
+            int index = 0;
+
+            for (int i = 0; i < haystack.Length; i++)
+            {
+                while (index > 0 && needle[index] != haystack[i])
+                { 
+                    index = pf[index - 1]; 
+                }
+
+                if (needle[index] == haystack[i]) 
+                    index++;
+
+                if (index == needle.Length)
+                {
+                    return i - index + 1;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
