d905845de10ac845055b0f588a2e0b2dd67f7084
IvanKrutik <ivan.krutik@gmail.com>
2023-12-24 16:24:46 +0400

    66. Plus One

---
+++ 66. Plus One/Helper.cs
@@ -0,0 +1,32 @@
+﻿using System.Numerics;
+
+namespace _66._Plus_One
+{
+    public static class Helper
+    {
+        public static int[] PlusOne(int[] digits)
+        {
+
+            string numText = string.Empty;
+            foreach (int digit in digits)
+            {
+                numText = numText + digit;
+            }
+            BigInteger num = BigInteger.Parse(numText);
+            num++;
+
+            numText = num.ToString();
+
+            int[] res = new int[numText.ToCharArray().Length];
+            var numArr = numText.ToCharArray();
+
+            for (int i = 0; i < numArr.Length; i++)
+            {
+                res[i] = int.Parse(numArr[i].ToString());
+            }
+
+            return res;
+
+        }
+    }
+}
