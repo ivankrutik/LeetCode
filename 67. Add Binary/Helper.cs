6c9d967eaede3efb0dbd612a7a9d490ed3a6b31f
IvanKrutik <ivan.krutik@gmail.com>
2023-12-24 19:22:38 +0400

    67. Add Binary
    Быстрый 98%, но съедает много памяти 5% )))

---
+++ 67. Add Binary/Helper.cs
@@ -0,0 +1,52 @@
+﻿namespace _67._Add_Binary
+{
+    public static class Helper
+    {
+        public static string AddBinary(string a, string b)
+        {
+            var aa = new string(a.Reverse().ToArray());
+            var bb = new string(b.Reverse().ToArray());
+
+            int aaLength = a.Length;
+            int bbLength = b.Length;
+            int length = Math.Max(a.Length, b.Length);
+            string res = string.Empty;
+
+            bool isDischarge = false;
+            int counter = 0;
+
+            for (int i = 0; i < length; i++)
+            {
+                if (aaLength > i && bbLength > i)
+                {
+                    counter = int.Parse(aa[i].ToString()) + int.Parse(bb[i].ToString()) + (isDischarge ? 1 : 0);
+                }
+                else if (aaLength > i)
+                {
+                    counter = int.Parse(aa[i].ToString()) + (isDischarge ? 1 : 0);
+                }
+                else if (bbLength > i)
+                {
+                    counter = int.Parse(bb[i].ToString()) + (isDischarge ? 1 : 0);
+                }
+
+                isDischarge = false;
+
+                if (counter >= 2)
+                {
+                    isDischarge = true;
+                    counter = counter % 2;
+                }
+
+                res = res + counter.ToString();
+            }
+            if (isDischarge)
+            {
+                res = res + "1";
+            }
+
+            return new string(res.Reverse().ToArray());
+            //return Convert.ToString((Convert.ToInt64(a, 2) + Convert.ToInt64(b, 2)), 2);
+        }
+    }
+}
---
+++ 67. Add Binary/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+ Given two binary strings a and b, return their sum as a binary string.
+
+ 
+
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+ 
+
+Constraints:
+
+1 <= a.length, b.length <= 104
+a and b consist only of '0' or '1' characters.
+Each string does not contain leading zeros except for the zero itself. 
+ */
+
+using _67._Add_Binary;
+
+//var a = "10100000100100110110010000010101111011011001101110111111111101000000101111001110001111100001101";
+//var b = "110101001011101110001111100110001010100001101011101010000011011011001011101111001100000011011110011";
+
+var a = "11";
+var b = "1";
+
+var res = Helper.AddBinary(a, b);
+
+Console.WriteLine(res);
