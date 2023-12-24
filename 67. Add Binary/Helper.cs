324bcd3b2f3bc4b7e13441409aa968777002eec4
IvanKrutik <ivan.krutik@gmail.com>
2023-12-24 19:30:28 +0400

    67. Add Binary
    StringBuilder - 20% memory

--- 67. Add Binary/Helper.cs
+++ 67. Add Binary/Helper.cs
@@ -1,33 +1,35 @@
-﻿namespace _67._Add_Binary
+﻿using System.Text;
+
+namespace _67._Add_Binary
 {
     public static class Helper
     {
         public static string AddBinary(string a, string b)
         {
-            var aa = new string(a.Reverse().ToArray());
-            var bb = new string(b.Reverse().ToArray());
+            a = new string(a.Reverse().ToArray());
+            b = new string(b.Reverse().ToArray());
 
             int aaLength = a.Length;
             int bbLength = b.Length;
             int length = Math.Max(a.Length, b.Length);
-            string res = string.Empty;
 
             bool isDischarge = false;
             int counter = 0;
+            StringBuilder sb = new StringBuilder();
 
             for (int i = 0; i < length; i++)
             {
                 if (aaLength > i && bbLength > i)
                 {
-                    counter = int.Parse(aa[i].ToString()) + int.Parse(bb[i].ToString()) + (isDischarge ? 1 : 0);
+                    counter = int.Parse(a[i].ToString()) + int.Parse(b[i].ToString()) + (isDischarge ? 1 : 0);
                 }
                 else if (aaLength > i)
                 {
-                    counter = int.Parse(aa[i].ToString()) + (isDischarge ? 1 : 0);
+                    counter = int.Parse(a[i].ToString()) + (isDischarge ? 1 : 0);
                 }
                 else if (bbLength > i)
                 {
-                    counter = int.Parse(bb[i].ToString()) + (isDischarge ? 1 : 0);
+                    counter = int.Parse(b[i].ToString()) + (isDischarge ? 1 : 0);
                 }
 
                 isDischarge = false;
@@ -38,14 +40,14 @@
                     counter = counter % 2;
                 }
 
-                res = res + counter.ToString();
+                sb.Append(counter.ToString());
             }
             if (isDischarge)
             {
-                res = res + "1";
+                sb.Append("1");
             }
+            return new string(sb.ToString().Reverse().ToArray());
 
-            return new string(res.Reverse().ToArray());
             //return Convert.ToString((Convert.ToInt64(a, 2) + Convert.ToInt64(b, 2)), 2);
         }
     }
