4bc6f8207fff28033c36bbd1cfbd898c42d4ab84
IvanKrutik <ivan.krutik@gmail.com>
2023-12-24 16:13:17 +0400

    58. Length of Last Word

---
+++ 58. Length of Last Word/Helper.cs
@@ -0,0 +1,17 @@
+﻿
+namespace _58._Length_of_Last_Word
+{
+    public static class Helper
+    {
+        public static int LengthOfLastWord(string s)
+        {
+            s = s.TrimEnd();
+            var arr = s.Split(' ');
+            if (arr.LastOrDefault() == null)
+            {
+                return 0;
+            }
+            return arr.LastOrDefault().Length;
+        }
+    }
+}
