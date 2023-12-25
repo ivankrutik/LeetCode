ce87094446cd6f18e42024bf7f552775fdb1b90c
IvanKrutik <ivan.krutik@gmail.com>
2023-12-25 15:41:26 +0400

    69. Sqrt(x)
    Binary Search

---
+++ 69. Sqrt(x)/Helper.cs
@@ -0,0 +1,30 @@
+﻿namespace _69._Sqrt_x_
+{
+    internal static class Helper
+    {
+        internal static int MySqrt(int x)
+        {
+            long left = 0;
+            long right = x;
+            long midle = 0;
+
+            while (left <= right)
+            {
+                midle = (left + right) / 2;
+                if (midle * midle < x)
+                {
+                    left = midle + 1;
+                }
+                else if (midle * midle > x)
+                {
+                    right = midle - 1;
+                }
+                else
+                {
+                    return (int)midle;
+                }
+            }
+            return (int)right;
+        }
+    }
+}
---
+++ 69. Sqrt(x)/Program.cs
@@ -0,0 +1,35 @@
+﻿/*
+ Given a non-negative integer x, return the square root of x rounded down to the nearest integer. 
+The returned integer should be non-negative as well.
+
+You must not use any built-in exponent function or operator.
+
+For example, do not use pow(x, 0.5) in c++ or x ** 0.5 in python.
+ 
+
+Example 1:
+
+Input: x = 4
+Output: 2
+Explanation: The square root of 4 is 2, so we return 2.
+
+
+Example 2:
+
+Input: x = 8
+Output: 2
+Explanation: The square root of 8 is 2.82842..., and since we round it down to the nearest integer, 2 is returned.
+ 
+
+Constraints:
+
+0 <= x <= 231 - 1
+ */
+
+using _69._Sqrt_x_;
+
+//var input = 8;
+var input = 2147395599;
+var output = Helper.MySqrt(input);
+
+Console.WriteLine(output);
