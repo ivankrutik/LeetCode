813568a87496e23b60a5a938ba31c0000b241c07
IvanKrutik <ivan.krutik@gmail.com>
2023-12-26 17:02:10 +0400

    70. Climbing Stairs - Ряд Фибоначи O(n)

---
+++ 70. Climbing Stairs/Helper.cs
@@ -0,0 +1,21 @@
+﻿namespace _70._Climbing_Stairs
+{
+    internal static class Helper
+    {
+        internal static int ClimbStairs(int n)
+        {
+            int[] fibonachies = new int[n + 3];
+            fibonachies[0] = 0;
+            fibonachies[1] = 1;
+            fibonachies[2] = 2;
+
+            //fill array
+            for (var i = 3; i <= n ; i++)
+            {
+                fibonachies[i] = fibonachies[i - 1] + fibonachies[i - 2];
+            }
+
+            return fibonachies[n];
+        }
+    }
+}
