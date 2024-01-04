60e38229397bc81e226af8adee2ea82725a19c05
IvanKrutik <ivan.krutik@gmail.com>
2024-01-04 19:00:08 +0400

    94. Binary Tree Inorder Traversal - recursion

---
+++ 94. Binary Tree Inorder Traversal/Helper.cs
@@ -0,0 +1,29 @@
+﻿namespace _94._Binary_Tree_Inorder_Traversal
+{
+    internal static class Helper
+    {
+        private static void getData(TreeNode node, ref IList<int> res)
+        {           
+            if (node.left != null)
+            {
+                getData(node.left, ref res);
+            }
+            res.Add(node.val);
+            if (node.right != null)
+            {
+                getData(node.right, ref res);
+            }
+        }
+
+        public static IList<int> InorderTraversal(TreeNode root)
+        {
+            IList<int> res = new List<int>();
+            if (root == null)
+                return res;
+
+            getData(root, ref res);
+
+            return res;
+        }
+    }
+}
---
+++ 94. Binary Tree Inorder Traversal/Program.cs
@@ -0,0 +1,59 @@
+﻿/*
+
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+
+Given the root of a binary tree, return the inorder traversal of its nodes' values.
+
+
+Example 1:
+Input: root = [1,null,2,3]
+Output: [1,3,2]
+
+Example 2:
+Input: root = []
+Output: []
+
+Example 3:
+Input: root = [1]
+Output: [1]
+ 
+
+Constraints:
+
+The number of nodes in the tree is in the range [0, 100].
+-100 <= Node.val <= 100
+ 
+
+Follow up: Recursive solution is trivial, could you do it iteratively?
+ */
+
+
+using _94._Binary_Tree_Inorder_Traversal;
+
+TreeNode root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.left.right.left = new TreeNode(8);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+root.right.right.left = new TreeNode(9);
+root.right.right.right = new TreeNode(10);
+
+var res = Helper.InorderTraversal(root);
+
+foreach (var val in res)
+{
+    Console.Write(val + " ");
+}
