c09ffd7c0ae9692b115af915bfb133f8f2115773
IvanKrutik <ivan.krutik@gmail.com>
2024-01-14 13:45:22 +0400

    100. Same Tree

---
+++ 100. Same Tree/Helper.cs
@@ -0,0 +1,47 @@
+﻿namespace _100._Same_Tree
+{
+    internal static class Helper
+    {
+        private static bool IsNotEqual;
+
+        private static void getData(TreeNode p, TreeNode q)
+        {
+
+
+            if (p.val != q.val ||
+                (p.left != null && q.left == null) ||
+                (p.left == null && q.left != null) ||
+                (p.right != null && q.right == null) ||
+                (p.right == null && q.right != null))
+            {
+                IsNotEqual = false;
+                return;
+            }
+
+            if (q.left != null && p.left != null)
+            {
+                getData(p.left, q.left);
+            }
+            if (q.right != null && p.right != null)
+            {
+                getData(p.right, q.right);
+            }
+        }
+
+        public static bool IsSameTree(TreeNode p, TreeNode q)
+        {
+            if (p == null && q == null)
+            {
+                return true;
+            }
+            if (p != null && q == null || p == null && q != null)
+            {
+                return false;
+            }
+
+            IsNotEqual = true;
+            getData(p, q);
+            return IsNotEqual;
+        }
+    }
+}
---
+++ 100. Same Tree/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ 
+Given the roots of two binary trees p and q, write a function to check if they are the same or not.
+Two binary trees are considered the same if they are structurally identical, and the nodes have the same value.
+
+ 
+Example 1:
+Input: p = [1,2,3], q = [1,2,3]
+Output: true
+
+Example 2:
+Input: p = [1,2], q = [1,null,2]
+Output: false
+
+Example 3:
+Input: p = [1,2,1], q = [1,1,2]
+Output: false
+ 
+
+Constraints:
+
+The number of nodes in both trees is in the range [0, 100].
+-104 <= Node.val <= 104
+
+ */
+
+using _100._Same_Tree;
+
+TreeNode root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(3);
+root.left.left = new TreeNode(4);
+root.left.right = new TreeNode(5);
+root.left.right.left = new TreeNode(8);
+root.right.left = new TreeNode(6);
+root.right.right = new TreeNode(7);
+root.right.right.left = new TreeNode(9);
+root.right.right.right = new TreeNode(10);
+
+TreeNode root2 = new TreeNode(1);
+root2.left = new TreeNode(33);
+root2.right = new TreeNode(3);
+root2.left.left = new TreeNode(4);
+root2.left.right = new TreeNode(5);
+root2.left.right.left = new TreeNode(8);
+root2.right.left = new TreeNode(6);
+root2.right.right = new TreeNode(7);
+root2.right.right.left = new TreeNode(9);
+root2.right.right.right = new TreeNode(10);
+
+var res = Helper.IsSameTree(root, root2);
+
+Console.WriteLine(res);
