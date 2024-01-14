263b395229947a009c758cf916f278123db29c70
IvanKrutik <ivan.krutik@gmail.com>
2024-01-14 14:06:15 +0400

    101. Symmetric Tree

---
+++ 101. Symmetric Tree/Helper.cs
@@ -0,0 +1,40 @@
+﻿namespace _101._Symmetric_Tree
+{
+    internal static class Helper
+    {
+        private static bool IsSymmetricTree;
+
+        private static void getData(TreeNode p, TreeNode q)
+        {
+            if (q == null && p == null)
+            {
+                return;
+            }
+            if (p != null && q == null || p == null && q != null)
+            {
+                IsSymmetricTree = false;
+                return;
+            }
+            if (p.val != q.val)
+            {
+                IsSymmetricTree = false;
+                return;
+            }
+
+            getData(p.left, q.right);
+            getData(p.right, q.left);
+        }
+
+        public static bool IsSymmetric(TreeNode root)
+        {
+            if (root == null)
+            {
+                return true;
+            }
+            IsSymmetricTree = true;
+            getData(root.left, root.right);
+            return IsSymmetricTree;
+        }
+
+    }
+}
---
+++ 101. Symmetric Tree/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+
+Given the root of a binary tree, check whether it is a mirror of itself (i.e., symmetric around its center).
+
+Example 1:
+Input: root = [1,2,2,3,4,4,3]
+Output: true
+
+Example 2:
+Input: root = [1,2,2,null,3,null,3]
+Output: false 
+
+Constraints:
+The number of nodes in the tree is in the range [1, 1000].
+-100 <= Node.val <= 100
+
+Follow up: Could you solve it both recursively and iteratively?
+
+ */
+using _101._Symmetric_Tree;
+
+TreeNode root = new TreeNode(1);
+root.left = new TreeNode(2);
+root.right = new TreeNode(2);
+root.left.left = new TreeNode(3);
+root.left.right = new TreeNode(4);
+root.right.left = new TreeNode(4);
+root.right.right = new TreeNode(3);
+
+var res = Helper.IsSymmetric(root);
+
+Console.WriteLine(res);
