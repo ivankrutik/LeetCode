70f409d502210d3472443b940504733cc42dff4b
IvanKrutik <ivan.krutik@gmail.com>
2024-01-15 17:01:51 +0400

    125. Valid Palindrome

---
+++ 125. Valid Palindrome/Helper.cs
@@ -0,0 +1,37 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _125._Valid_Palindrome
+{
+    internal static class Helper
+    {
+        public static bool IsPalindrome(string s)
+        {
+            if (s.Length == 1)
+            {
+                return true;
+            }
+            s = Regex.Replace(s, "[-.?!)(\\s,:;'@#_{}\\[\\]\"`]", "").ToLower();
+
+            if (s.Length == 0)
+            {
+                return true;
+            }
+
+            int startIndex = 0;
+            int endIndex = s.Length - 1;
+            while (true)
+            {
+                if (s[startIndex] != s[endIndex])
+                {
+                    return false;
+                }
+                if (startIndex >= endIndex)
+                {
+                    return true;
+                }
+                startIndex++;
+                endIndex--;
+            }
+        }
+    }
+}
