a7f7e4af108b5126ba648893883a46feb0ec5a8a
IvanKrutik <ivan.krutik@gmail.com>
2024-01-18 18:59:56 +0400

    136. Single Number - побитовое исключающее «или»

---
+++ 136. Single Number/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _136._Single_Number
+{
+    internal static class Helper
+    {
+        public static int SingleNumber(int[] nums)
+        {
+            int res = 0;
+            for (int i = 0; i < nums.Length; i++) 
+            {
+                res ^= nums[i];
+            }
+            return res;
+        }
+    }
+}
---
+++ 190. Reverse Bits/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace _190._Reverse_Bits
+{
+    internal static class Helper
+    {
+        public static uint reverseBits(uint n)
+        {
+
+        }
+    }
+}
