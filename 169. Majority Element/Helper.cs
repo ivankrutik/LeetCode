c5270ef3234ec8d64e5e45b4b1f46c61449b997c
IvanKrutik <ivan.krutik@gmail.com>
2024-01-19 18:39:22 +0400

    169. Majority Element - LINQ is not sports

---
+++ 169. Majority Element/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _169._Majority_Element
+{
+    internal static class Helper
+    {
+        public static int MajorityElement(int[] nums)
+        {
+            var res = nums.GroupBy(x => x).
+                Select(z => new { ValueData = z, Count = z.Count() }).
+                FirstOrDefault(x => x.Count == nums.GroupBy(x => x).
+                Select(z => new { ValueData = z, Count = z.Count() }).
+                Max(z => z.Count)).ValueData.FirstOrDefault();
+            return res;
+        }
+    }
+}
