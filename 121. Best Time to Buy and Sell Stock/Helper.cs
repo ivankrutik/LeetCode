1da8c7ecdb5378706afa5a202480bf4bfb473def
IvanKrutik <ivan.krutik@gmail.com>
2024-01-19 18:17:33 +0400

    121. Best Time to Buy and Sell Stock - фиксируем день покупки

---
+++ 121. Best Time to Buy and Sell Stock/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace _121._Best_Time_to_Buy_and_Sell_Stock
+{
+    internal static class Helper
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            var res = 0;
+            int prevMin = int.MaxValue;
+            foreach (var pr in prices) 
+            {
+                res = Math.Max(res, pr - prevMin);
+                prevMin = Math.Min(prevMin, pr);
+            }
+                       
+            return res;
+        }
+    }
+}
--- 190. Reverse Bits/Helper.cs
+++ 190. Reverse Bits/Helper.cs
@@ -1,6 +1,6 @@
 ﻿namespace _190._Reverse_Bits
 {
-    internal static class Helper
+    public static class Helper
     {
         public static uint reverseBits(uint n)
         {
