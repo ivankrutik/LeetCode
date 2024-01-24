229cde7e8fcced33021b1552ac534ca6cabf9c78
IvanKrutik <ivan.krutik@gmail.com>
2024-01-24 21:07:21 +0400

    160. Intersection of Two Linked Lists

---
+++ 160. Intersection of Two Linked Lists/Helper.cs
@@ -0,0 +1,42 @@
+﻿namespace _160._Intersection_of_Two_Linked_Lists
+{
+    internal static class Helper
+    {
+        public static ListNode GetIntersectionNode(ListNode headA, ListNode headB)
+        {
+            if (headA == null || headB == null)
+            {
+                return null;
+            }
+
+            var nodeA = headA;
+            var nodeB = headB;
+
+            while (true)
+            {
+                if (nodeA == nodeB)
+                {
+                    return nodeA;
+                }
+                nodeA = nodeA.next;
+                nodeB = nodeB.next;
+                if (nodeA == nodeB)
+                {
+                    return nodeA;
+                }
+
+                if (nodeA == null )
+                {
+                    nodeA = headB;
+                    continue;
+                }
+                if (nodeB == null )
+                {
+                    nodeB = headA;
+                    continue;
+                }
+               
+            }
+        }
+    }
+}
---
+++ 160. Intersection of Two Linked Lists/ListNode.cs
@@ -0,0 +1,13 @@
+﻿namespace _160._Intersection_of_Two_Linked_Lists
+{
+    internal class ListNode
+    {
+        public int val;
+        public ListNode? next;
+
+        public ListNode(int x)
+        {
+            val = x;
+        }
+    }
+}
