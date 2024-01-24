5f2808724a3e66e1d480f162300bd7b6e9e5dc24
IvanKrutik <ivan.krutik@gmail.com>
2024-01-24 21:50:06 +0400

    206. Reverse Linked List - Stack

---
+++ 206. Reverse Linked List/Helper.cs
@@ -0,0 +1,39 @@
+﻿namespace _206._Reverse_Linked_List
+{
+    using System.Collections.Generic;
+
+    internal static class Helper
+    {
+        public static ListNode ReverseList(ListNode head)
+        {
+            if (head == null)
+            {
+                return null;
+                  }
+            Stack<ListNode> stack = new Stack<ListNode>();
+            while (true)
+            {
+                if (head == null)
+                {
+                    break;
+                }
+                stack.Push(head);
+                head = head.next;
+            }
+            head = stack.Pop();
+            var temp = head;
+            while (true)
+            {
+                if (!stack.Any())
+                {
+                    temp.next = null;
+                    break;
+                }
+                temp.next = stack.Pop();
+                temp = temp.next;
+            }
+
+            return head;
+        }
+    }
+}
