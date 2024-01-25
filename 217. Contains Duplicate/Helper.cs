e2417ed2ecba337ed0336cac4b09f0e776bb408c
IvanKrutik <ivan.krutik@gmail.com>
2024-01-25 16:50:55 +0400

    217. Contains Duplicate - HashSet

---
+++ 217. Contains Duplicate/Helper.cs
@@ -0,0 +1,18 @@
+﻿namespace _217._Contains_Duplicate
+{
+    internal static class Helper
+    {
+        public static bool ContainsDuplicate(int[] nums)
+        {
+            HashSet<int> keyValuePairs = new HashSet<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (keyValuePairs.Contains(nums[i]))
+                    return true;
+                else
+                    keyValuePairs.Add(nums[i]);
+            }
+            return false;
+        }
+    }
+}
