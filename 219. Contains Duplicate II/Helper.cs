417853e2fc36e06b3ed419b3b3757ac6c69e41fc
IvanKrutik <ivan.krutik@gmail.com>
2024-01-25 18:15:59 +0400

    219. Contains Duplicate II

---
+++ 219. Contains Duplicate II/Helper.cs
@@ -0,0 +1,30 @@
+﻿namespace _219._Contains_Duplicate_II
+{
+    internal static class Helper
+    {
+        public static bool ContainsNearbyDuplicate(int[] nums, int k)
+        {
+            Dictionary<int, int> keyValuePairs = new Dictionary<int, int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (keyValuePairs.ContainsValue(nums[i]))
+                {
+                    if ((i - keyValuePairs.Where(x=>x.Value == nums[i]).FirstOrDefault().Key) <= k)
+                    {
+                        return true;
+                    }
+                }
+                if (keyValuePairs.Count <= k)
+                {
+                    keyValuePairs.Add(i, nums[i]);
+                }
+                else
+                {
+                    keyValuePairs.Remove(i - k - 1);
+                    keyValuePairs.Add(i, nums[i]);
+                }
+            }
+            return false;
+        }
+    }
+}
