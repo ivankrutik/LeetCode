6e8c367a0c50eb6b7b25d8df99a5c7b300067016
IvanKrutik <ivan.krutik@gmail.com>
2024-01-29 21:44:12 +0400

    232. Implement Queue using Stacks - Stack FIFO

---
+++ 232. Implement Queue using Stacks/MyQueue.cs
@@ -0,0 +1,58 @@
+﻿namespace _232._Implement_Queue_using_Stacks
+{
+    public class MyQueue
+    {
+        private Stack<int> _in;
+        private Stack<int> _out;
+
+        public MyQueue()
+        {
+            _in = new Stack<int>();
+            _out = new Stack<int>();
+        }
+
+        public void Push(int x)
+        {
+            _in.Push(x);
+        }
+
+        public int Pop()
+        {
+            if (_out.Any())
+            {
+                return _out.Pop();
+            }
+            while (true)
+            {
+                if (_in.Count == 0)
+                    break;
+
+                _out.Push(_in.Pop());
+            }
+            return _out.Pop();
+        }
+
+        public int Peek()
+        {
+            if (_out.Any())
+            {
+                return _out.Peek();
+            }
+            while (true)
+            {
+                if (_in.Count == 0)
+                    break;
+
+                _out.Push(_in.Pop());
+            }
+            return _out.Peek();
+        }
+
+        public bool Empty()
+        {
+            if (_in.Any() || _out.Any())
+                return false;
+            else return true;
+        }
+    }
+}
