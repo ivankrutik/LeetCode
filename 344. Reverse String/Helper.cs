4057ede405ed9170a685887de96f806fc4a19c46
IvanKrutik <ivan.krutik@gmail.com>
2024-01-30 19:10:01 +0400

    344. Reverse String - pointer. O(n)

---
+++ 344. Reverse String/Helper.cs
@@ -0,0 +1,21 @@
+﻿namespace _344._Reverse_String
+{
+    internal static class Helper
+    {
+        public static char[] ReverseString(char[] s)
+        {
+            var left = 0;
+            var right = s.Length-1;
+            char temp;
+            while (left < right) 
+            {
+                temp = s[left];
+                s[left] = s[right];
+                s[right] = temp;
+                left++;
+                right--;
+            }
+            return s;
+        }
+    }
+}
---
+++ 344. Reverse String/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ * Write a function that reverses a string. The input string is given as an array of characters s.
+You must do this by modifying the input array in-place with O(1) extra memory. 
+
+Example 1:
+Input: s = ["h","e","l","l","o"]
+Output: ["o","l","l","e","h"]
+
+Example 2:
+Input: s = ["H","a","n","n","a","h"]
+Output: ["h","a","n","n","a","H"] 
+
+Constraints:
+
+1 <= s.length <= 105
+s[i] is a printable ascii character.
+ */
+using _344._Reverse_String;
+
+var s = new char[] { 'h', 'e', 'l', 'l', 'o' };
+var res = Helper.ReverseString(s);
+
+foreach (char c in res)
+{
+    Console.Write(c.ToString() + " ");
+}
