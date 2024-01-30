30263e7cd35d189c9d5195f5344f2b002ec4ca87
IvanKrutik <ivan.krutik@gmail.com>
2024-01-30 16:50:43 +0400

    196. Delete Duplicate Emails

---
+++ 196. Delete Duplicate Emails/query.sql
@@ -0,0 +1,55 @@
+Table: Person
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| id          | int     |
+| email       | varchar |
++-------------+---------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains an email. The emails will not contain uppercase letters.
+ 
+
+Write a solution to delete all duplicate emails, keeping only one unique email with the smallest id.
+
+For SQL users, please note that you are supposed to write a DELETE statement and not a SELECT one.
+
+For Pandas users, please note that you are supposed to modify Person in place.
+
+After running your script, the answer shown is the Person table. The driver will first compile and run your piece of code and then show the Person table. The final order of the Person table does not matter.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Person table:
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
+| 3  | john@example.com |
++----+------------------+
+Output: 
++----+------------------+
+| id | email            |
++----+------------------+
+| 1  | john@example.com |
+| 2  | bob@example.com  |
++----+------------------+
+Explanation: john@example.com is repeated two times. We keep the row with the smallest Id = 1.
+
+
+
+
+
+/* Write your PL/SQL query statement below */
+delete 
+from Person p 
+where not exists 
+            (select null from (select p0.email, min(p0.id) as min_id
+                                    from Person p0 
+                                    group by p0.email)r  where p.email = r.email and p.id = r.min_id )
