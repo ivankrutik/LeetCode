70f587580be12b7426c5f57646825fd42e40648d
IvanKrutik <ivan.krutik@gmail.com>
2024-01-30 18:41:27 +0400

    268. Missing Number  -  Арифмитическая прогрессия

---
+++ 268. Missing Number/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _268._Missing_Number
+{
+    internal static class Helper
+    {
+        public static int MissingNumber(int[] nums)
+        {
+            var sum = nums.Length * (nums.Length+1) / 2;
+            foreach (var i in nums) 
+            {
+                sum -= i;
+            }
+            return sum;
+        }
+    }
+}
