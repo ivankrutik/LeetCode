3d270f3334cbfc27c09637d866ac28105eea8b63
IvanKrutik <ivan.krutik@gmail.com>
2024-02-03 20:46:04 +0400

    231. Power of Two - бинарные операции
    2 в степени - 1 и 00000000  - n-(е) количество 0.

---
+++ 231. Power of Two/Helper.cs
@@ -0,0 +1,10 @@
+﻿namespace _231._Power_of_Two
+{
+    internal class Helper
+    {
+        public static bool IsPowerOfTwo(int n)
+        {
+            return n > 0 && (n & (n - 1)) == 0;
+        }
+    }
+}
