0716c855fef62e38a1b3884621e16cbee1c4dc72
IvanKrutik <ivan.krutik@gmail.com>
2024-02-03 19:49:40 +0400

    225. Implement Stack using Queues

---
+++ 225. Implement Stack using Queues/MyStack.cs
@@ -0,0 +1,64 @@
+﻿using System.Security.AccessControl;
+
+namespace _225._Implement_Stack_using_Queues
+{
+    public class MyStack
+    {
+        private Queue<int> _queue { get; set; }
+        private Queue<int> _queueTemp { get; set; }
+
+        private void ReplaceQueue()
+        {
+            if (!_queue.Any())
+            {
+                while (_queueTemp.Any())
+                {
+                    _queue.Enqueue(_queueTemp.Dequeue());
+                }
+                while (_queue.Count != 1)
+                {
+                    _queueTemp.Enqueue(_queue.Dequeue());
+                }
+            }
+            else
+            {
+                while (_queue.Count != 1)
+                {
+                    _queueTemp.Enqueue(_queue.Dequeue());
+                }
+            }
+        }
+
+        public MyStack()
+        {
+            _queue = new Queue<int>();
+            _queueTemp = new Queue<int>();
+        }
+
+        public void Push(int x)
+        {
+            _queue.Enqueue(x);
+        }
+
+        public int Pop()
+        {
+            ReplaceQueue();
+            return _queue.Dequeue();
+        }
+
+        public int Top()
+        {
+            ReplaceQueue();
+            return _queue.Peek();
+        }
+
+        public bool Empty()
+        {
+            if (_queue.Any() || _queueTemp.Any())
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
