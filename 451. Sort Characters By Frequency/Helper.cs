b5b474b921f08da9e91ce0a8727b52839efceac3
IvanKrutik <ivan.krutik@gmail.com>
2024-02-07 18:15:41 +0400

    451. Sort Characters By Frequency

---
+++ 451. Sort Characters By Frequency/Helper.cs
@@ -0,0 +1,34 @@
+﻿using System.Text;
+
+namespace _451._Sort_Characters_By_Frequency
+{
+    internal static class Helper
+    {
+        public static string FrequencySort(string s)
+        {
+            var dict = new Dictionary<char, int>();
+
+            foreach (char c in s)
+            {
+                if (dict.ContainsKey(c))
+                {
+                    dict[c]++;
+                }
+                else
+                {
+                    dict.Add(c, 1);
+                }
+            }
+            var sb = new StringBuilder();
+            foreach (var c in dict.OrderByDescending(x => x.Value))
+            {
+                for (var i = 0; i < c.Value; i++) 
+                {
+                    sb.Append(c.Key);
+                }                
+            }
+
+            return sb.ToString();
+        }
+    }
+}
