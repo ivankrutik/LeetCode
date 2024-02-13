53b89c90755632bc409760f177e23e5a24d7afeb
IvanKrutik <ivan.krutik@gmail.com>
2024-02-13 18:59:40 +0400

    448. Find All Numbers Disappeared in an Array - отсортировать массив к номеру его позиции

---
+++ 448. Find All Numbers Disappeared in an Array/Helper.cs
@@ -0,0 +1,30 @@
+﻿namespace _448._Find_All_Numbers_Disappeared_in_an_Array
+{
+    internal static class Helper
+    {
+        public static IList<int> FindDisappearedNumbers(int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                while (true)
+                {
+                    var j = nums[i] - 1;
+                    if (nums[i] == nums[j])
+                    {
+                        break;
+                    }
+                    (nums[i], nums[j]) = (nums[j], nums[i]);
+                }
+            }
+            var res = new List<int>();
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] != i + 1)
+                {
+                    res.Add(i+1);
+                }
+            }
+            return res;
+        }
+    }
+}
---
+++ 448. Find All Numbers Disappeared in an Array/Program.cs
@@ -0,0 +1,28 @@
+﻿/*
+Given an array nums of n integers where nums[i] is in the range [1, n], 
+return an array of all the integers in the range [1, n] that do not appear in nums.
+
+Example 1:
+Input: nums = [4,3,2,7,8,2,3,1]
+Output: [5,6]
+
+Example 2:
+Input: nums = [1,1]
+Output: [2]
+ 
+Constraints:
+
+n == nums.length
+1 <= n <= 105
+1 <= nums[i] <= n
+
+Follow up: Could you do it without extra space and in O(n) runtime? You may assume the returned list does not count as extra space.
+ */
+using _448._Find_All_Numbers_Disappeared_in_an_Array;
+
+var nums = new int[] { 4, 3, 2, 7, 8, 2, 3, 1 };
+var res = Helper.FindDisappearedNumbers(nums);
+foreach (var i in res)
+{
+    Console.Write(i.ToString() + " ");
+}
