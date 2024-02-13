371070164b8d8546eac75df4ec2856d9ef3e5625
IvanKrutik <ivan.krutik@gmail.com>
2024-02-13 13:34:00 +0400

    2108. Find First Palindromic String in the Array - easy

---
+++ 2108. Find First Palindromic String in the Array/Helper.cs
@@ -0,0 +1,32 @@
+﻿namespace _2108._Find_First_Palindromic_String_in_the_Array
+{
+    internal static class Helper
+    {
+        private static bool IsPalindrome(string word)
+        {
+            var l = 0;
+            var r = word.Length - 1;
+
+            while (l <= r)
+            {
+                if (word[l] != word[r])
+                {
+                    return false;
+                }
+                l++;
+                r--;
+            }
+            return true;
+        }
+
+        public static string FirstPalindrome(string[] words)
+        {
+            foreach (string word in words)
+            {
+                if (IsPalindrome(word))
+                    return word;
+            }
+            return string.Empty;
+        }
+    }
+}
