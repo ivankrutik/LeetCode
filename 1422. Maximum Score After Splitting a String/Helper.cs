801d5c9e3d93b57df937612bd6ca7084b3ebea6d
IvanKrutik <ivan.krutik@gmail.com>
2024-02-17 20:00:32 +0400

    1422. Maximum Score After Splitting a String
    Интересное решение. Бежим по значениям и считаем максимальное результирующее.

---
+++ 1422. Maximum Score After Splitting a String/Helper.cs
@@ -0,0 +1,22 @@
+﻿namespace _1422._Maximum_Score_After_Splitting_a_String
+{
+    internal static class Helper
+    {
+        public static int MaxScore(string s)
+        {
+            var left = 0;
+            var right = s.Where(x => x == '1').Count();
+            var score = 0;
+            for (var i = 0; i < s.Length - 1; i++)
+            {
+                if (s[i] == '0')
+                    left++;
+                else
+                    right--;
+
+                score = Math.Max(score, left + right);
+            }
+            return score;
+        }
+    }
+}
