a1aad5a4563ab4d11b5e0a44747c65e8bb1bbc62
IvanKrutik <ivan.krutik@gmail.com>
2024-02-18 17:56:49 +0400

    1408. String Matching in an Array - не сложно, но решение не быстрое.
    Думаю, дело в стандартных методах.

---
+++ 1408. String Matching in an Array/Helper.cs
@@ -0,0 +1,23 @@
+﻿namespace _1408._String_Matching_in_an_Array
+{
+    internal static class Helper
+    {
+        public static IList<string> StringMatching(string[] words)
+        {
+            var res = new List<string>();
+            words = words.OrderByDescending(x => x.Length).ToArray();
+            for (int i = 0; i < words.Length; i++)
+            {
+                for (int j = i-1; j >= 0; j--)
+                {
+                    if (words[j].Contains(words[i]))
+                    {
+                        res.Add(words[i]);
+                        break;
+                    }
+                }
+            }
+            return res;
+        }
+    }
+}
