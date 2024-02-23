8bf0460b5903d2b7ef0ea2a58bd4f3c467f4fedf
IvanKrutik <ivan.krutik@gmail.com>
2024-02-23 12:30:23 +0400

    104. Maximum Depth of Binary Tree

---
+++ 104. Maximum Depth of Binary Tree/Helper.cs
@@ -0,0 +1,30 @@
+﻿namespace _104._Maximum_Depth_of_Binary_Tree
+{
+    internal static class Helper
+    {
+        private static int _depth = 0;
+
+        private static void GetChild(TreeNode node, int level)
+        {
+            if (node == null)
+                return;
+
+            _depth = Math.Max(_depth, level + 1);
+            if (node.left != null)
+            {
+                GetChild(node.left, level + 1);
+            }
+            if (node.right != null)
+            {
+                GetChild(node.right, level + 1);
+            }
+            return;
+        }
+
+        public static int MaxDepth(TreeNode root)
+        {
+            GetChild(root, 0);
+            return _depth;
+        }
+    }
+}
