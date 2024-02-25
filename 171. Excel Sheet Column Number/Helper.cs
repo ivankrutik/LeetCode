e1f131d49a9d7dba608f30257a82b031acd99eed
IvanKrutik <ivan.krutik@gmail.com>
2024-02-25 21:56:29 +0400

    171. Excel Sheet Column Number - обратная 168

---
+++ 171. Excel Sheet Column Number/Helper.cs
@@ -0,0 +1,19 @@
+﻿namespace _171._Excel_Sheet_Column_Number
+{
+    internal static class Helper
+    {
+        public static int TitleToNumber(string columnTitle)
+        {
+            int columnNumber= 0;
+
+            for (var i = 0; i < columnTitle.Length; i++)
+            {
+                var t = Convert.ToInt32(columnTitle[i] + 1) - 65;
+                var temp = t * Math.Pow(26, columnTitle.Length - i - 1);
+                columnNumber = (int)((int)columnNumber + temp);
+            }
+
+            return columnNumber;
+        }
+    }
+}
