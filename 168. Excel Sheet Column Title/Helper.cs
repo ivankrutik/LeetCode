2851df155dd2325a37cecd281b55b1a8b84840a1
IvanKrutik <ivan.krutik@gmail.com>
2024-02-25 21:19:22 +0400

    168. Excel Sheet Column Title. Хоть и нет спецалгоритма, мне не показалась легкой (((

---
+++ 168. Excel Sheet Column Title/Helper.cs
@@ -0,0 +1,24 @@
+﻿namespace _168._Excel_Sheet_Column_Title
+{
+    using System.Text;
+
+    internal static class Helper
+    {
+        public static string ConvertToTitle(int columnNumber)
+        {
+            var revers = string.Empty;
+            StringBuilder res = new StringBuilder();
+            while (columnNumber > 0)
+            {
+                revers += Convert.ToChar((columnNumber - 1) % 26 + 65);
+                columnNumber = (columnNumber - 1) / 26;
+            }
+            for (var i = revers.Length - 1; i >= 0; i--)
+            {
+                res.Append(revers[i]);
+            }
+
+            return res.ToString();
+        }
+    }
+}
