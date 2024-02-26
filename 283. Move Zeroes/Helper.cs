95027b263aee8ab316cd977c03a29f5234f3d990
IvanKrutik <ivan.krutik@gmail.com>
2024-02-26 21:31:57 +0400

    283. Move Zeroes - O(n) , обычная замена элементов

---
+++ 283. Move Zeroes/Helper.cs
@@ -0,0 +1,26 @@
+﻿namespace _283._Move_Zeroes
+{
+    internal static class Helper
+    {
+        public static int[] MoveZeroes(int[] nums)
+        {
+            for (var i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    for (var j = i; j < nums.Length; j++)
+                    {
+                        if (nums[j] != 0)
+                        {                            
+                            nums[i] = nums[j];
+                            nums[j] = 0;
+                            break;
+                        }
+                    }
+                }
+            }
+
+            return nums;
+        }
+    }
+}
