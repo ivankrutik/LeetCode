0ce4758d3cf748d333d706688ba3847e8dab3b1a
IvanKrutik <ivan.krutik@gmail.com>
2024-02-27 17:58:52 +0400

    278. First Bad Version - BinarySearch

---
+++ 278. First Bad Version/Helper.cs
@@ -0,0 +1,39 @@
+﻿namespace _278._First_Bad_Version
+{
+    public class Helper : VersionControl
+    {
+        public int FirstBadVersion(int n)
+        {
+            long left = 0;
+            long right = n;
+            long position = 0;
+
+            while (left < right)
+            {
+                if ((right + left) / 2 == left || (right + left) / 2 == right)
+                {
+                    position = left + 1;
+                }
+                else
+                {
+                    position = (right + left) / 2;
+                }
+                
+                if (this.IsBadVersion((int)position))
+                {
+                    if (right == position)
+                    {
+                        break;
+                    }
+                    right = position;
+                }
+                else
+                {
+                    left = position;
+                }
+            }
+
+            return (int)position;
+        }
+    }
+}
---
+++ 278. First Bad Version/VersionControl.cs
@@ -0,0 +1,13 @@
+﻿namespace _278._First_Bad_Version
+{
+    public class VersionControl
+    {
+        public bool IsBadVersion(int version)
+        {
+            if (version >= 1702766719)
+                return true;
+
+            return false ;
+        }
+    }
+}
