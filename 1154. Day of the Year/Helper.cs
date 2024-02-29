d2cefe066664c79e443e4d9c95d6b6d4a78e5354
IvanKrutik <ivan.krutik@gmail.com>
2024-02-29 18:42:42 +0400

    1154. Day of the Year
    Наверное надо было делать хитро, но я что-то этого не понял

---
+++ 1154. Day of the Year/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _1154._Day_of_the_Year
+{
+    internal static class Helper
+    {
+        public static int DayOfYear(string date)
+        {
+            int yy = int.Parse(date.Split('-')[0].ToString());
+            int mm = int.Parse(date.Split('-')[1].ToString());
+            int dd = int.Parse(date.Split('-')[2].ToString());
+
+            var datetime = new DateTime(year: yy, month: mm, day: dd);
+            return datetime.DayOfYear;
+        }
+    }
+}
