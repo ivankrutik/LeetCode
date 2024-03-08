984a03b8cdfd03f487f5f7f1ddcaea4cee66be1b
IvanKrutik <ivan.krutik@gmail.com>
2024-03-08 19:40:17 +0400

    3005. Count Elements With Maximum Frequency. LINQ
    LINQ - это всегда медлено и дорого

---
+++ 3005. Count Elements With Maximum Frequency/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace _3005._Count_Elements_With_Maximum_Frequency
+{
+    internal static class Helper
+    {
+        public static int MaxFrequencyElements(int[] nums)
+        {
+            var res = nums.GroupBy(x => x).Select(group => new { Value = group.Key, Count = group.Count() });
+            var r = res.Where(w=>w.Count == res.Max(m=>m.Count)).Sum(s=>s.Count);
+            return r;
+        }
+    }
+}
---
+++ 476. Number Complement/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _476._Number_Complement
+{
+    internal class Helper
+    {
+        public int FindComplement(int num)
+        {
+            int rep = 0;
+            int count = 0;
+
+
+
+            return 0;
+        }
+    }
+}
