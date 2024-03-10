496ae54b96c9bc3e7f704950fc06ccf4d1839c69
IvanKrutik <ivan.krutik@gmail.com>
2024-03-10 17:59:32 +0400

    415. Add Strings - массивы съедают много памяти, а в целом работает

---
+++ 415. Add Strings/Helper.cs
@@ -0,0 +1,60 @@
+﻿using System.Text;
+
+namespace _415._Add_Strings
+{
+    internal static class Helper
+    {
+        public static string AddStrings(string num1, string num2)
+        {
+            var maxLength = Math.Max(num1.Length, num2.Length);
+
+            int[] arr1 = new int[maxLength];
+            int[] arr2 = new int[maxLength];
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                if (num1.Length > i)
+                {
+                    arr1[i] = int.Parse(num1[num1.Length - i - 1].ToString());
+                }
+                else
+                {
+                    arr1[i] = 0;
+                }
+
+                if (num2.Length > i)
+                {
+                    arr2[i] = int.Parse(num2[num2.Length - i - 1].ToString());
+                }
+                else
+                {
+                    arr2[i] = 0;
+                }
+            }
+
+            Boolean isOverflow = false;
+            var res = new StringBuilder();
+
+            for (int i = 0; i < maxLength; i++)
+            {
+                var sum = arr1[i] + arr2[i] + (isOverflow ? 1 : 0);
+                if (sum >= 10)
+                {
+                    isOverflow = true;
+                    sum = sum - 10;
+                }
+                else
+                {
+                    isOverflow = false;
+                }
+                res.Append(sum.ToString());
+            }
+            if (isOverflow)
+            {
+                res.Append("1");
+            }
+
+            return new string(res.ToString().Reverse().ToArray());
+        }
+    }
+}
