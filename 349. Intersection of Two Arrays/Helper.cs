0ea71e120ede9b8f5a38804b6e92354046d3c57e
IvanKrutik <ivan.krutik@gmail.com>
2024-03-10 16:21:11 +0400

    349. Intersection of Two Arrays - примитивная сложность.
    По скорости очень быстро 97%, по памяти лучше 35%

---
+++ 349. Intersection of Two Arrays/Helper.cs
@@ -0,0 +1,27 @@
+﻿namespace _349._Intersection_of_Two_Arrays
+{
+    internal static class Helper
+    {
+        public static int[] Intersection(int[] nums1, int[] nums2)
+        {
+            HashSet<int> set = new HashSet<int>();
+            var res = new List<int>();
+            foreach (int n1 in nums1)
+            {
+                if (!set.Contains(n1))
+                {
+                    set.Add(n1);
+                }
+            }
+
+            foreach (int n2 in nums2)
+            {
+                if (set.Contains(n2) && !res.Contains(n2))
+                {
+                    res.Add(n2);
+                }
+            }
+            return res.ToArray();
+        }
+    }
+}
