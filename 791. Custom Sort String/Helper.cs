b0d38256b2be297f267a6fc705b8dd5515ab5ce8
IvanKrutik <ivan.krutik@gmail.com>
2024-03-11 19:32:10 +0400

    791. Custom Sort String - PriorityQueue
    Быстро, но больше чем другие по памяти из-за очереди приоритетов

---
+++ 791. Custom Sort String/Helper.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace _791._Custom_Sort_String
+{
+    internal static class Helper
+    {
+        public static string CustomSortString(string order, string s)
+        {
+            Dictionary<char, int> dict = new Dictionary<char, int>();
+            for (int i = 0; i < order.Length; i++)
+            {
+                dict.Add(order[i], i+1);
+            }
+
+            PriorityQueue<char, int> priorityQueue = new PriorityQueue<char, int>();
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (dict.ContainsKey(s[i]))
+                {
+                    priorityQueue.Enqueue(s[i], dict[s[i]]);
+                }
+                else
+                {
+                    priorityQueue.Enqueue(s[i], 0);
+                }
+            }
+
+            var sb = new StringBuilder();
+            while (priorityQueue.Count != 0)
+            {
+                sb.Append(priorityQueue.Dequeue());
+            }
+            return sb.ToString();
+        }
+    }
+}
