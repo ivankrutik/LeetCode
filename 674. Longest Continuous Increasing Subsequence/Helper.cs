c0edbc694eb37cb6bcc06100f808462a22fa04ab
IvanKrutik <ivan.krutik@gmail.com>
2024-03-12 18:48:43 +0400

    674. Longest Continuous Increasing Subsequence
    Банально, нет никаких алгоритмов и прочее.

---
+++ 674. Longest Continuous Increasing Subsequence/Helper.cs
@@ -0,0 +1,22 @@
+﻿namespace _674._Longest_Continuous_Increasing_Subsequence
+{
+    internal static class Helper
+    {
+        public static int FindLengthOfLCIS(int[] nums)
+        {
+            int res = 1;
+            int total = 1;
+            for (int i = 0; i < nums.Length - 1; i++)
+            {
+                if (nums[i] < nums[i + 1])
+                {
+                    res = res + 1;
+                    total = Math.Max(res, total);
+                }
+                else
+                    res = 1;
+            }
+            return total;
+        }
+    }
+}
