cb1ae03242aed6f6d64e626ee0ce97e5fe9bd6f7
IvanKrutik <ivan.krutik@gmail.com>
2024-03-17 20:24:04 +0400

    525. Contiguous Array - интересный алгоритм

---
+++ 525. Contiguous Array/Helper.cs
@@ -0,0 +1,36 @@
+﻿namespace _525._Contiguous_Array
+{
+    internal static class Helper
+    {
+        public static int FindMaxLength(int[] nums)
+        {
+            Dictionary<int, int> dict = new Dictionary<int, int>();
+            dict.Add(0, -1);
+
+            int max = 0;
+            int count = 0;
+
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == 0)
+                {
+                    count--;
+                }
+                else
+                {
+                    count++;
+                }
+
+                if (dict.ContainsKey(count))
+                {
+                    max = Math.Max(max, i - dict[count]);
+                }
+                else
+                {
+                    dict.Add(count, i);
+                }
+            }
+            return max;
+        }
+    }
+}
