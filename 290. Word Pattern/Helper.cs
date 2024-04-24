2d0d9317e4566d61e8e666e288d6662186794230
IvanKrutik <ivan.krutik@gmail.com>
2024-04-24 18:33:04 +0400

    290. Word Pattern - Биекция

---
+++ 290. Word Pattern/Helper.cs
@@ -0,0 +1,36 @@
+﻿namespace _290._Word_Pattern
+{
+    internal static class Helper
+    {
+        public static bool WordPattern(string pattern, string s)
+        {
+            Dictionary<char, string> dict = new Dictionary<char, string>();
+            var arr = s.Split(' ');
+            if (arr.Length != pattern.Length)
+            {
+                return false;
+            }
+
+            for (int i = 0; i < pattern.Length; i++)
+            {
+                if (dict.ContainsKey(pattern[i]))
+                {
+                    if (dict[pattern[i]] != arr[i])
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    if (dict.ContainsValue(arr[i]))
+                    {
+                        return false;
+                    }
+                    dict.Add(pattern[i], arr[i]);
+                }
+            }
+
+            return true;
+        }
+    }
+}
