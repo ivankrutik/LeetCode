844a5b6e13b512c0656247543c3e4f535911a018
IvanKrutik <ivan.krutik@gmail.com>
2024-04-24 19:11:49 +0400

    1137. N-th Tribonacci Number - Трибоначи

---
+++ 1137. N-th Tribonacci Number/Helper.cs
@@ -0,0 +1,24 @@
+﻿namespace _1137._N_th_Tribonacci_Number
+{
+    internal static class Helper
+    {
+        public static int Tribonacci(int n)
+        {
+            long[] dict = new long[n+3];
+            dict[0] = 0;
+            dict[1] = 1;
+            dict[2] = 1;
+
+            if (n <= 2)
+            {
+                return (int)dict[n];
+            }
+
+            for (int i = 3; i <= n; i++) 
+            {
+                dict[i] = dict[i-1] + dict[i-2] + dict[i-3];
+            }
+            return (int)dict[n];
+        }
+    }
+}
