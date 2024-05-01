613a8fef369ea7e88c0d29e990b184c2134cc868
IvanKrutik <ivan.krutik@gmail.com>
2024-05-01 14:16:20 +0400

    2000. Reverse Prefix of Word - Easy. Stack.

---
+++ 2000. Reverse Prefix of Word/Helper.cs
@@ -0,0 +1,43 @@
+﻿using System.Text;
+
+namespace _2000._Reverse_Prefix_of_Word
+{
+    internal static class Helper
+    {
+        public static string ReversePrefix(string word, char ch)
+        {
+            Stack<char> stack = new Stack<char>();
+            int? posExist = null;
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                if (word[i] != ch)
+                {
+                    stack.Push(word[i]);
+                }
+                else
+                {
+                    stack.Push(word[i]);
+                    posExist = i;
+                    break;
+                }
+            }
+            if (posExist == null)
+            {
+                return word;
+            }
+
+            while (stack.Any())
+            {
+                sb.Append(stack.Pop());
+            }
+            for (int i = (int)posExist + 1; i < word.Length; i++)
+            {
+                sb.Append(word[i]);
+            }
+
+            return sb.ToString();
+        }
+    }
+}
