de447438a53e6cb1a9f48a21c730f1c9ed45179d
IvanKrutik <ivan.krutik@gmail.com>
2024-05-01 14:30:59 +0400

    258. Add Digits - Easy. Числовой корень.

---
+++ 258. Add Digits/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _258._Add_Digits
+{
+    internal static class Helper
+    {
+        public static int AddDigits(int num)
+        {
+            if (num == 0) 
+                return 0;
+
+            var r = num % 9;
+
+            return r == 0 ? 9 : r;
+        }
+    }
+}
