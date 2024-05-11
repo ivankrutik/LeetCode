0c20a518d35bdd99bc8d53208287f4c647fc58fe
IvanKrutik <ivan.krutik@gmail.com>
2024-05-11 13:44:42 +0400

    292. Nim Game. Упрощенная версия игры. Одна кучка, каждая 4я позиция проигрышная.

---
+++ 292. Nim Game/Helper.cs
@@ -0,0 +1,12 @@
+﻿namespace _292._Nim_Game
+{
+    internal static class Helper
+    {
+        public static bool CanWinNim(int n)
+        {
+            if (n % 4 == 0)
+            { return false; }
+            return true;
+        }
+    }
+}
