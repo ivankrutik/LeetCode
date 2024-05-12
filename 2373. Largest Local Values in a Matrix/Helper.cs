f51f30cbb9f0f3fdc05320fd21dc7c774df8d068
IvanKrutik <ivan.krutik@gmail.com>
2024-05-12 12:24:28 +0400

    2373. Largest Local Values in a Matrix

---
+++ 2373. Largest Local Values in a Matrix/Helper.cs
@@ -0,0 +1,36 @@
+﻿namespace _2373._Largest_Local_Values_in_a_Matrix
+{
+    internal static class Helper
+    {
+        private static int getLocalMax(int[][] grid, int i, int j)
+        {
+            var res = grid[i][j];
+            res = Math.Max(res, grid[i - 1][j]);
+            res = Math.Max(res, grid[i + 1][j]);
+            res = Math.Max(res, grid[i][j + 1]);
+            res = Math.Max(res, grid[i][j - 1]);
+            res = Math.Max(res, grid[i - 1][j - 1]);
+            res = Math.Max(res, grid[i - 1][j + 1]);
+            res = Math.Max(res, grid[i + 1][j - 1]);
+            res = Math.Max(res, grid[i + 1][j + 1]);
+
+            return res;
+        }
+
+        public static int[][] LargestLocal(int[][] grid)
+        {
+            var res = new int[grid.Length - 2][];
+
+            for (var i = 1; i < grid.Length - 1; i++)
+            {
+                res[i - 1] = new int[grid.Length - 2];
+                for (var j = 1; j < grid.Length - 1; j++)
+                {
+                    res[i - 1][j - 1] = getLocalMax(grid, i, j);
+                }
+            }
+
+            return res;
+        }
+    }
+}
