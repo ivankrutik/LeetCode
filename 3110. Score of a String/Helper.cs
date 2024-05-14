a82da02f64ceb011e376ead53e2bdad01dba1e89
Ivan Krutik <ivan.krutik@gmail.com>
2024-05-14 14:58:04 +0400

    3110. Score of a String - Очень Easy

---
+++ 3110. Score of a String/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _3110._Score_of_a_String
+{
+    internal static class Helper
+    {
+        public static int ScoreOfString(string s)
+        {
+            int sum = 0;
+            for (int i = 0; i < s.Length-1; i++)
+            {
+                sum = sum + Math.Abs(s[i] - s[i + 1]);
+            }
+            return sum;
+        }
+    }
+}
