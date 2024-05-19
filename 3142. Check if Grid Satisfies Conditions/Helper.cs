03c7e0d7aa6b26fddefb336b8a315ea90cdbc82f
IvanKrutik <ivan.krutik@gmail.com>
2024-05-19 11:50:03 +0400

    3142. Check if Grid Satisfies Conditions - EASY

---
+++ 3142. Check if Grid Satisfies Conditions/Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace _3142._Check_if_Grid_Satisfies_Conditions
+{
+    internal static class Helper
+    {
+        public static bool SatisfiesConditions(int[][] grid)
+        {
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[i].Length; j++)
+                {
+                    if (((i + 1) < grid.Length && grid[i][j] != grid[i + 1][j]) || ((j + 1) < grid[i].Length && grid[i][j] == grid[i][j + 1]))
+                    {
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
+    }
+}
