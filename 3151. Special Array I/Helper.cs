ede4d29133d6482f7aaa4b843f94d08d937bedf4
IvanKrutik <ivan.krutik@gmail.com>
2024-05-19 11:38:06 +0400

    3151. Special Array I - EASY

---
+++ 3151. Special Array I/Helper.cs
@@ -0,0 +1,23 @@
+﻿namespace _3151._Special_Array_I
+{
+    internal static class Helper
+    {
+        public static bool IsArraySpecial(int[] nums)
+        {
+            bool isEven = nums[0] % 2 == 0;
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if ((isEven && nums[i] % 2 != 0) || (!isEven && nums[i] % 2 == 0))
+                {
+                    isEven = nums[i] % 2 == 0;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
