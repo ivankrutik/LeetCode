dcfd9aa8a4b4299dd4619b5deec99a83df6421cd
Ivan Krutik <ivan.krutik@gmail.com>
2024-05-27 16:26:33 +0400

    1608. Special Array With X Elements Greater Than or Equal X (EASY) Нет конкретного алгоритма

---
+++ 1608. Special Array With X Elements Greater Than or Equal X/Helper.cs
@@ -0,0 +1,31 @@
+﻿namespace _1608._Special_Array_With_X_Elements_Greater_Than_or_Equal_X
+{
+    internal static class Helper
+    {
+        public static int SpecialArray(int[] nums)
+        {
+            nums = nums.Order().ToArray();
+            var r = -1;
+
+            for (var i = nums.Length; i > 0; i--)
+            {
+                var isExist = true;
+                for (var j = 0; j < i; j++)
+                {
+                    if (nums[nums.Length - j - 1] < i)
+                    {
+                        isExist = false;
+                        break;
+                    }
+                }
+                if (isExist &&  (nums.Length - i -1 < 0 || nums[nums.Length - i - 1] < i))
+                {
+                    r = i;
+                    break;
+                }
+            }
+
+            return r;
+        }
+    }
+}
