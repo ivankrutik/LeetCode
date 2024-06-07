88e32793c5b129f8b8535ba58e143215438bed52
Ivan Krutik <ivan.krutik@gmail.com>
2024-06-07 11:58:53 +0400

    648. Replace Words - Medium. Tier. Поиск корня слова. Интересная структура данных.
    Trie или префиксное дерево — это особый вид дерева поиска, в котором для ключей узлов обычно используются строки. Префиксные деревья могут использоваться, например, для реализации множеств и ассоциативных массивов, но по-настоящему они проявляют себя при обходе и поиске ключей, начинающихся с определенного префикса.1 июл. 2022 г.

---
+++ 648. Replace Words/Classes/TLconst.cs
@@ -0,0 +1,9 @@
+﻿namespace _648._Replace_Words.Classes
+{
+    public static class TLconst
+    {
+        public static readonly
+           int
+           asciiTrieLimit = 256; // A Trie contains characters from the standard ASCII code.
+    }// TLconst (static class)
+}
