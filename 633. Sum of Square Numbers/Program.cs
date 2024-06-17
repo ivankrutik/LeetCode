80ef28b489e96d818b9a4f33033b9f790debd0c0
Ivan Krutik <ivan.krutik@gmail.com>
2024-06-17 10:51:32 +0400

    633. Sum of Square Numbers

---
+++ 633. Sum of Square Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿/*
+ 633. Sum of Square Numbers
+Medium
+Topics
+Companies
+Given a non-negative integer c, decide whether there're two integers a and b such that a2 + b2 = c.
+
+Example 1:
+Input: c = 5
+Output: true
+Explanation: 1 * 1 + 2 * 2 = 5
+
+Example 2:
+Input: c = 3
+Output: false
+
+Constraints:
+
+0 <= c <= 231 - 1
+ */
+using _633._Sum_of_Square_Numbers;
+
+var c = 2147483600;
+var r = Helper.JudgeSquareSum(c);
+
+Console.WriteLine(r);
