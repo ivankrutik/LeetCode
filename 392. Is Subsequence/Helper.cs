e0fc1391b599322d3072828e23b815d388fcac34
Ivan Krutik <ivan.krutik@gmail.com>
2024-06-17 17:38:32 +0400

    392. Is Subsequence - EASY. Метод двух указателей

---
+++ 392. Is Subsequence/Helper.cs
@@ -0,0 +1,25 @@
+﻿namespace _392._Is_Subsequence
+{
+    internal static class Helper
+    {
+        public static bool IsSubsequence(string s, string t)
+        {
+            int countMath = 0;
+            int index = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                while (index < t.Length)
+                {
+                    if (s[i] == t[index])
+                    {
+                        index++;
+                        countMath++;
+                        break;
+                    }
+                    index++;
+                }
+            }
+            return countMath == s.Length;
+        }
+    }
+}
