e81106b314ecee30a3273b4c5fb4da069c4a345b
Ivan Krutik <ivan.krutik@gmail.com>
2024-06-19 18:27:45 +0400

    122. Best Time to Buy and Sell Stock II - MEDIUM. Без алгоритма, элементарная.

---
+++ 122. Best Time to Buy and Sell Stock II/Helper.cs
@@ -0,0 +1,20 @@
+﻿namespace _122._Best_Time_to_Buy_and_Sell_Stock_II
+{
+    internal static class Helper
+    {
+        public static int MaxProfit(int[] prices)
+        {
+            int sum = 0;
+
+            for (int i = prices.Length - 1; i >= 0; i--)
+            {
+                if (i - 1 >= 0 && prices[i] > prices[i - 1])
+                {
+                    sum += prices[i] - prices[i - 1];
+                }
+            }
+
+            return sum;
+        }
+    }
+}
