bf793ba5aff6177a33848018fac57c4b3649f5e3
Ivan Krutik <ivan.krutik@gmail.com>
2024-07-22 15:55:19 +0400

    2418. Sort the People. Easy. Нет четкого алгоритма.

---
+++ 2418. Sort the People/Helper.cs
@@ -0,0 +1,15 @@
+﻿namespace _2418._Sort_the_People
+{
+    internal static class Helper
+    {
+        public static string[] SortPeople(string[] names, int[] heights)
+        {
+            var dict = new Dictionary<int, string>();
+            for (int i = 0; i < names.Length; i++) 
+            {
+                dict.Add(heights[i], names[i]);
+            }
+            return dict.OrderByDescending(x=>x.Key).Select(x=>x.Value).ToArray();
+        }
+    }
+}
