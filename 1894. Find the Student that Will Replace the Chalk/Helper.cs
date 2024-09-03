09477ecf1402c5cf378f9a77015abe9281ddccdc
Ivan Krutik <ivan.krutik@gmail.com>
2024-09-03 10:48:18 +0400

    1894. Find the Student that Will Replace the Chalk. Medium. Плохое решение. Задача на BigInteger.

---
+++ 1894. Find the Student that Will Replace the Chalk/Helper.cs
@@ -0,0 +1,25 @@
+﻿namespace _1894._Find_the_Student_that_Will_Replace_the_Chalk
+{
+    internal static class Helper
+    {
+        public static int ChalkReplacer(int[] chalk, int k)
+        {
+            int i = 0;
+            while (k - chalk[i] >= 0)
+            {
+                k = k - chalk[i];
+
+                if (i + 1 == chalk.Length)
+                {
+                    i = 0;
+                    continue;
+                }
+                else
+                {
+                    i++;
+                }
+            }
+            return i;
+        }
+    }
+}
