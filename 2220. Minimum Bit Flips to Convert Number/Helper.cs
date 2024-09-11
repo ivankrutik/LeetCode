703dba6f03ccb24ea155defdd17a0aa61a354820
Ivan Krutik <ivan.krutik@gmail.com>
2024-09-11 16:01:12 +0400

    2220. Minimum Bit Flips to Convert Number. EASY. XOR.

---
+++ 2220. Minimum Bit Flips to Convert Number/Helper.cs
@@ -0,0 +1,11 @@
+﻿namespace _2220._Minimum_Bit_Flips_to_Convert_Number
+{
+    internal static class Helper
+    {
+        public static int MinBitFlips(int start, int goal)
+        {
+            var res = ((uint)(start ^ goal));
+            return Convert.ToString(res, toBase: 2).Count(c => c == '1');
+        }
+    }
+}
