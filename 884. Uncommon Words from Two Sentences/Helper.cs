2ba7371704e5ecdba4ba135b1bf8bb33bb6d0dfe
Ivan Krutik <ivan.krutik@gmail.com>
2024-09-17 17:42:09 +0400

    884. Uncommon Words from Two Sentences. EASY.

---
+++ 884. Uncommon Words from Two Sentences/Helper.cs
@@ -0,0 +1,26 @@
+﻿namespace _884._Uncommon_Words_from_Two_Sentences
+{
+    internal static class Helper
+    {
+        public static string[] UncommonFromSentences(string s1, string s2)
+        {
+            Dictionary<string, string> dictAll = new Dictionary<string, string>();
+            Dictionary<string, string> dictUk = new Dictionary<string, string>();
+
+            foreach (string word in (s1 + " " + s2).Split(" "))
+            {
+                if (!dictAll.ContainsKey(word))
+                {
+                    dictUk.Add(word, word);
+                    dictAll.Add(word, word);
+                }
+                else
+                {
+                    dictUk.Remove(word);
+                }
+            }
+
+            return dictUk.Keys.ToArray();
+        }
+    }
+}
