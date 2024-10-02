15cdd1217c17e304ba0eec2b2f2978db0dc8464f
Ivan Krutik <ivan.krutik@gmail.com>
2024-10-02 10:58:33 +0400

    1331. Rank Transform of an Array. EASY.

---
+++ 1331. Rank Transform of an Array/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+
+namespace _1331._Rank_Transform_of_an_Array
+{
+    internal static class Helper
+    {
+        public static int[] ArrayRankTransform(int[] arr)
+        {
+            var dict = new List<Tuple<int, int>>();
+            for (var i = 0; i < arr.Length; i++)
+            {
+                dict.Add(new Tuple<int, int>(i, arr[i]));
+            }
+
+            var sortedDict = from entry in dict orderby entry.Item2, entry.Item1 ascending select entry;
+
+            var r = sortedDict.ToList();
+            var res = new int[r.Count];
+
+            int counter = -1;
+            for (var i = 0; i < r.Count; i++)
+            {
+                if (i > 0 && r[i].Item2 == r[i - 1].Item2)
+                {
+                    res[r[i].Item1] = counter + 1;
+                    continue;
+                }
+                counter++;
+                res[r[i].Item1] = counter + 1;
+            }
+
+            return res;
+        }
+    }
+}
