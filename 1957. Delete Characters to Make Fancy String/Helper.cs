9cee1c7fc14fc84be995398b674f194566e81f01
Ivan Krutik <ivan.krutik@gmail.com>
2024-11-01 10:13:59 +0400

    1957. Delete Characters to Make Fancy String. EASY. Нет конкретного алгоритма. Работа со строками.

---
+++ 1957. Delete Characters to Make Fancy String/Helper.cs
@@ -0,0 +1,32 @@
+﻿namespace _1957._Delete_Characters_to_Make_Fancy_String
+{
+    internal static class Helper
+    {
+        public static string MakeFancyString(string s)
+        {
+            Queue<char> queue = new Queue<char>();
+
+            int count = 0;
+            char lastChar = '\0';
+            for (int i = 0; i < s.Length; i++) 
+            {
+                if (lastChar == s[i])
+                {
+                    count++;
+                    if (count >2)
+                    {
+                        continue;
+                    }
+                }
+                else
+                {
+                    count = 1;
+                }
+                lastChar = s[i];
+                queue.Enqueue(s[i]);
+            }
+
+            return new string( queue.ToArray());
+        }
+    }
+}
