66e5092f5524fe18b8f83a2ce88427075f01cf1f
Ivan Krutik <ivan.krutik@gmail.com>
2024-11-07 18:26:55 +0400

    2707._Extra_Characters_in_a_String. Medium. Dynamic programming. Не проходит часть тестов по скорости.

---
+++ 2707. Extra Characters in a String/Helper.cs
@@ -0,0 +1,27 @@
+﻿namespace _2707._Extra_Characters_in_a_String
+{
+    public static class Helper
+    {
+        private static int Total = int.MaxValue;
+
+        private static void RemoveWord(string s, string[] dictionary)
+        {
+            Total = Math.Min(Total, s.Where(z => z != '_').Count());
+
+            foreach (string word in dictionary)
+            {
+                if (s.Contains(word))
+                {
+                    var s2 = s.Replace(word, "_");
+                    RemoveWord(s2, dictionary);
+                }
+            }
+        }
+
+        public static int MinExtraChar(string s, string[] dictionary)
+        {
+            RemoveWord(s, dictionary);
+            return Total;
+        }
+    }
+}
