b7e801ec20da482710e859085d55813e6fb6f429
Ivan Krutik <ivan.krutik@gmail.com>
2024-11-17 16:09:42 +0400

    118. Pascal's Triangle. Треугольник Паскаля. EASY. Интуитивно понятно.

---
+++ 118. Pascal's Triangle/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+Example 1:
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+
+Example 2:
+Input: numRows = 1
+Output: [[1]]
+
+Constraints:
+
+1 <= numRows <= 30
+*/
+
+using _118._Pascal_s_Triangle;
+
+var numRows = 5;
+var r = Helper.Generate(numRows);
+
+foreach (var row in r)
+{
+    Console.WriteLine();
+    foreach (var col in r)
+    {
+        Console.WriteLine(col);
+    }
+}
+Console.WriteLine("Hello, World!");
