13c57df3e88c8fd5f59b4dfe82628a691950f7b1
Ivan Krutik <ivan.krutik@gmail.com>
2024-12-13 11:33:02 +0400

    2593. Find Score of an Array After Marking All Elements. Medium. Алгоритм работы с массивом. Получилось быстрое исполнение.

---
+++ 2593. Find Score of an Array After Marking All Elements/Helper.cs
@@ -0,0 +1,34 @@
+﻿namespace _2593._Find_Score_of_an_Array_After_Marking_All_Elements
+{
+    internal static class Helper
+    {
+        public static long FindScore(int[] nums)
+        {
+            long score = 0;
+
+            var dict = new Dictionary<int, int>();
+            int i = 0;
+            foreach (var num in nums)
+            {
+                dict.Add(i++, num);
+            }
+            dict = dict.OrderBy(x => x.Value).ThenBy(x => x.Key).ToDictionary();
+
+            foreach (var d in dict)
+            {
+
+                if (nums[d.Key] != 0)
+                {
+                    score += nums[d.Key];
+                    nums[d.Key] = 0;
+                    if (d.Key + 1 < nums.Length)
+                        nums[d.Key + 1] = 0;
+                    if (d.Key - 1 >= 0)
+                        nums[d.Key - 1] = 0;
+                }
+            }
+
+            return score;
+        }
+    }
+}
