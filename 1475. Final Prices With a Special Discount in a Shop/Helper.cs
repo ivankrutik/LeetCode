7994615810fa93006dc5b834d2edf22862079508
Ivan Krutik <ivan.krutik@gmail.com>
2024-12-18 10:16:31 +0400

    1475. Final Prices With a Special Discount in a Shop. EASY. BrootForce.

---
+++ 1475. Final Prices With a Special Discount in a Shop/Helper.cs
@@ -0,0 +1,21 @@
+﻿namespace _1475._Final_Prices_With_a_Special_Discount_in_a_Shop
+{
+    internal static class Helper
+    {
+        public static int[] FinalPrices(int[] prices)
+        {
+            for (int i = 0; i < prices.Length; i++)
+            {
+                for (int j = i+1; j < prices.Length; j++)
+                {
+                    if (prices[i] >= prices[j])
+                    { 
+                        prices[i] = prices[i] - prices[j];
+                        break;
+                    }
+                }
+            }
+            return prices;
+        }
+    }
+}
