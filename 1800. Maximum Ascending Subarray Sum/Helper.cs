338095ab1c59326df64326dfe2ec6a3e42720701
Ivan Krutik <ivan.krutik@gmail.com>
2025-02-04 12:31:31 +0400

    1800. Maximum Ascending Subarray Sum. EASY.

---
+++ 1800. Maximum Ascending Subarray Sum/Helper.cs
@@ -0,0 +1,25 @@
+﻿namespace _1800._Maximum_Ascending_Subarray_Sum
+{
+    public static class Helper
+    {
+        public static int MaxAscendingSum(int[] nums)
+        {
+            int sumPlus = nums[0];
+            int tempPlus = nums[0];
+
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i - 1] < nums[i])
+                {
+                    tempPlus = tempPlus + nums[i];
+                }
+                else
+                    tempPlus = nums[i];
+
+                sumPlus = Math.Max(sumPlus, tempPlus);
+            }
+
+            return sumPlus;
+        }
+    }
+}
