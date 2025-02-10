1ce8a7229275d13fd58bd9db167e3c818304baee
Ivan Krutik <ivan.krutik@gmail.com>
2025-02-10 14:17:57 +0400

    3174. Clear Digits. EASY. STACK.

---
+++ 3174. Clear Digits/Helper.cs
@@ -0,0 +1,23 @@
+﻿namespace _3174._Clear_Digits
+{
+    public static class Helper
+    {
+        public static string ClearDigits(string s)
+        {
+            var chars = new Stack<char>();
+
+            foreach (char c in s)
+            {
+                if ('0' <= c && c <= '9')
+                {
+                    if (chars.Any())
+                        chars.Pop();
+                }
+                else
+                    chars.Push(c);
+            }
+
+            return new string( chars.Reverse().ToArray());
+        }
+    }
+}
