4d35c5f3cbf694747a68eee58b26531700e35e8a
IvanKrutik <ivan.krutik@gmail.com>
2026-04-24 15:34:43 +0400

    2833. Furthest Point From Origin (EASY)

---
+++ 2833. Furthest Point From Origin/Helper.cs
@@ -0,0 +1,24 @@
+﻿namespace _2833._Furthest_Point_From_Origin
+{
+    public static class Helper
+    {
+        public static int FurthestDistanceFromOrigin(string moves)
+        {
+            int result = 0;
+            int counter = 0;
+
+            foreach (var move in moves) 
+            {
+                if (move == 'L')
+                    result --;
+                if (move == 'R')
+                    result ++;
+                if (move == '_')
+                    counter ++;
+            }
+            result = Math.Abs(result) + counter;
+
+            return result;
+        }
+    }
+}
---
+++ 2833. Furthest Point From Origin/Program.cs
@@ -0,0 +1,6 @@
+﻿
+using _2833._Furthest_Point_From_Origin;
+
+var moves = "_R__LL_";
+
+Console.WriteLine(Helper.FurthestDistanceFromOrigin(moves));
