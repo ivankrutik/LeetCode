feba7ee44fd33446245c5b16d24cf60921063cb4
IvanKrutik <ivan.krutik@gmail.com>
2026-04-24 15:47:20 +0400

    387. First Unique Character in a String (EASY)

---
+++ 387. First Unique Character in a String/Helper.cs
@@ -0,0 +1,31 @@
+﻿namespace _387._First_Unique_Character_in_a_String
+{
+    public static class Helper
+    {
+        public static int FirstUniqChar(string s)
+        {
+            HashSet<char> set = new HashSet<char>();
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (set.Contains(s[i])) continue;
+                
+                bool IsMath = false;
+                for (int j = i + 1; j < s.Length; j++)
+                {
+                    if (s[i] == s[j])
+                    {
+                        IsMath = true;
+                        set.Add(s[i]);
+                        break;
+                    }
+                }
+
+                if (!IsMath)
+                    return i;
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ 387. First Unique Character in a String/Program.cs
@@ -0,0 +1,8 @@
+﻿
+
+
+using _387._First_Unique_Character_in_a_String;
+
+
+var s = "aabb";
+Console.WriteLine(Helper.FirstUniqChar(s));
