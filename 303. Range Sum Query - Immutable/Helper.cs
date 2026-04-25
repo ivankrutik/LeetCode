99adc695d95a7c0d8369535c5fa2875e578fcdab
IvanKrutik <ivan.krutik@gmail.com>
2026-04-25 14:24:23 +0400

    303. Range Sum Query - Immutable (EASY)
    Алгоритм префиксной суммы, на основе предобработки массива чисел для нахождения суммы диапозона значений.

---
+++ 303. Range Sum Query - Immutable/Helper.cs
@@ -0,0 +1,27 @@
+﻿namespace _303._Range_Sum_Query___Immutable
+{
+    public static class Helper
+    {
+        static int runningSum = 0;
+        static int[]? prefixSum;
+
+        public static void NumArray(int[] nums)
+        {
+            prefixSum = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                runningSum += nums[i];
+                prefixSum[i] = runningSum;
+            }
+        }
+
+        public static int SumRange(int left, int right)
+        {
+            if (left == 0)
+                return prefixSum[right];
+
+            return prefixSum[right] - prefixSum[left - 1];
+        }
+
+    }
+}
